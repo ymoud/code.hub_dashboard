74e5c858c6f0064a9a1d810a934296138409c4af
Yannis Moudilos <Yannis@YANNIS-LAPTOP>
2018-10-27 18:01:48 +0300

    - adds logic to dashboard component - adds common folder

--- app/containers/Dashboard.jsx
+++ app/containers/Dashboard.jsx
@@ -1,15 +1,53 @@
 import React from "react";
+import Services from "../services/index";
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      stats: [],
+      courses: [],
+      isLoaded: false,
+    };
     this.displayName = "Dashboard Component";
   }
 
+  componentDidMount() {
+    Services.getStats()
+      .then(response => {
+        this.setState(state => {
+          state.stats = response.data;
+          return state;
+        });
+      });
+
+    Services.getCourses()
+      .then(response => {
+        this.setState(state => {
+          //TODO: sort by dates.start_date DESC, then filter first 5
+          state.courses = response.data;
+          return state;
+        });
+      });
+  }
+
   render() {
+    const {stats, courses} = this.state;
+
     return (
-      <h2>Dashboard</h2>
+      <React.Fragment>
+        <h2>Dashboard</h2>
+        {
+          stats && stats.length 
+            ? stats.map(statItem => <p key={statItem.id}>{statItem.title}</p>)
+            : <p>No data found</p>
+        }
+        {
+          courses && courses.length 
+            ? courses.map(course => <p key={courses.id}>{course.title}</p>)
+            : <p>No courses found</p>
+        }
+      </React.Fragment>
     );
   }
 }
